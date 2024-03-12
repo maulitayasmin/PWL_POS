9892435253cfc2b0fbb7de63672dbecda02fec41
Maulita Yasmin Nadila <maulitayasmin2@gmail.com>
2024-03-13 02:31:42 +0700

    praktikum2.6 js4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,7 +6,7 @@
     <body>
         <h1>Data User</h1>
         <table border="1" cellpading="2" cellspacing="0">
-            <tr>
+            {{-- <tr>
                 <th>ID</th>
                 <th>Usernama</th>
                 <th>Nama</th>
@@ -18,8 +18,27 @@
                 <td>{{ $data->username }}</td>
                 <td>{{ $data->nama }}</td>
                 <td>{{ $data->level_id }}</td>
-            </tr>
+            </tr> --}}
 
+            <tr>
+                <th>ID</th>
+                <th>Usernama</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
+                <th>Aksi</th>
+            </tr>
+            @foreach ($data as $d)
+                <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td>
+                    <a href="/user/ubah/{{ $d->user_id }}">Ubah</a>
+                    <a href="/user/hapus/{{ $d->user_id }}">Hapus</a>
+                </td>
+            </tr>
+            @endforeach
         </table>
     </body>
 </html>
