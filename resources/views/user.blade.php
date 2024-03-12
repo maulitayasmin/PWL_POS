5a9c0a7f2e94250d9f9454a6b5933aa1b0b95195
Maulita Yasmin Nadila <maulitayasmin2@gmail.com>
2024-03-12 14:34:46 +0700

    praktikum2.4 js4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,19 +7,17 @@
         <h1>Data User</h1>
         <table border="1" cellpading="2" cellspacing="0">
             <tr>
-                <th>Jumlah Pengguna</th>
-                {{-- <th>ID</th>
+                <th>ID</th>
                 <th>Usernama</th>
                 <th>Nama</th>
-                <th>ID Level Pengguna</th> --}}
+                <th>ID Level Pengguna</th>
             </tr>
 
             <tr>
-                <td>{{ $data }}</td>
-                {{-- <td>{{ $data->user_id }}</td>
+                <td>{{ $data->user_id }}</td>
                 <td>{{ $data->username }}</td>
                 <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td> --}}
+                <td>{{ $data->level_id }}</td>
             </tr>
 
         </table>
