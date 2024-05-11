35f710879f7a53f9cca371f901609dfc04e23f98
Maulita Yasmin Nadila <maulitayasmin2@gmail.com>
2024-05-11 22:57:48 +0700

    F. METHOD MOVE JS12

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -11,21 +11,17 @@ class FileUploadController extends Controller
     }
 
     public function prosesFileUpload(Request $request){
-        // dump($request->berkas);
-        // dump($request->file('file'));
-        // return "Pemrosesan file upload di sini";
+        
         $request->validate([
             'berkas'=>'required|file|image|max:500',]);
-            // $path = $request->berkas->store('uploads');
-            // $namaFile=$request->berkas->getClientOriginalName(); // fungsi untuk mengambil nama file
             $extfile=$request->berkas->getClientOriginalName();
             $namaFile='web-'.time().".".$extfile;
-            // $path = $request->berkas->storeAs('uploads', $namaFile);
-            // $path = $request->berkas->storeAs('uploads', 'berkas');
-            $path = $request->berkas->storeAs('public', $namaFile);
-            // echo "proses upload berhasil, file berada di: $path";
-            // echo $request->berkas->getClientOriginalName()."lolos validasi";
-            $pathbaru=asset('storage/'.$namaFile);
+
+            $path = $request->berkas->move('gambar', $namaFile);
+            $path = str_replace("\\","//", $path);
+            echo "Variabel path berisi:$path <br>";
+
+            $pathbaru=asset('gambar/'.$namaFile);
             echo "proses upload berhasil, data disimpan pada:$path";
             echo "<br>";
             echo "Tampilkan link:<a href='$pathbaru'>$pathbaru</a>";
