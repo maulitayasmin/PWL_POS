691b171a9294ac5034e4878886d181d86a3b80f1
Maulita Yasmin Nadila <maulitayasmin2@gmail.com>
2024-05-11 22:32:08 +0700

    D. MEMINDAH FILE UPLOAD & E. MEMBUAT SYMLINK JS12

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -14,9 +14,20 @@ class FileUploadController extends Controller
         // dump($request->berkas);
         // dump($request->file('file'));
         // return "Pemrosesan file upload di sini";
-
         $request->validate([
             'berkas'=>'required|file|image|max:500',]);
-        echo $request->berkas->getClientOriginalName()."lolos validasi";
+            // $path = $request->berkas->store('uploads');
+            // $namaFile=$request->berkas->getClientOriginalName(); // fungsi untuk mengambil nama file
+            $extfile=$request->berkas->getClientOriginalName();
+            $namaFile='web-'.time().".".$extfile;
+            // $path = $request->berkas->storeAs('uploads', $namaFile);
+            // $path = $request->berkas->storeAs('uploads', 'berkas');
+            $path = $request->berkas->storeAs('public', $namaFile);
+            // echo "proses upload berhasil, file berada di: $path";
+            // echo $request->berkas->getClientOriginalName()."lolos validasi";
+            $pathbaru=asset('storage/'.$namaFile);
+            echo "proses upload berhasil, data disimpan pada:$path";
+            echo "<br>";
+            echo "Tampilkan link:<a href='$pathbaru'>$pathbaru</a>";
     }
 }
