d2928f13b30e6a417f68ccb21eae83f31632928e
Maulita Yasmin Nadila <maulitayasmin2@gmail.com>
2024-05-12 00:03:49 +0700

    TUGAS JS12

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -6,24 +6,30 @@ use Illuminate\Http\Request;
 
 class FileUploadController extends Controller
 {
-    public function fileUpload(){
+    public function fileUpload()
+    {
         return view('file-upload');
     }
 
-    public function prosesFileUpload(Request $request){
-        
+    public function prosesFileUpload(Request $request)
+    {
         $request->validate([
-            'berkas'=>'required|file|image|max:500',]);
-            $extfile=$request->berkas->getClientOriginalName();
-            $namaFile='web-'.time().".".$extfile;
+            'berkas' => 'required|file|image|max:500', // Validasi file gambar
+            'image_name' => 'required|string', // Validasi inputan nama file
+        ]);
 
-            $path = $request->berkas->move('gambar', $namaFile);
-            $path = str_replace("\\","//", $path);
-            echo "Variabel path berisi:$path <br>";
+        // Mendapatkan ekstensi file yang diunggah
+        $extfile = $request->berkas->getClientOriginalExtension();
 
-            $pathbaru=asset('gambar/'.$namaFile);
-            echo "proses upload berhasil, data disimpan pada:$path";
-            echo "<br>";
-            echo "Tampilkan link:<a href='$pathbaru'>$pathbaru</a>";
+        // Mendapatkan nama file akhir dari inputan teks
+        $namaFile = $request->input('image_name'). '.' . $extfile;
+
+        // Menyimpan file dengan nama yang ditentukan
+        $path = $request->berkas->move('gambar', $namaFile);
+
+        // Menampilkan informasi dan link file yang diunggah
+        echo "Gambar berhasil diupload ke <a href='$path' target='_blank'>$namaFile</a>"; // Menambahkan link ke gambar
+        echo "<br> <br>";
+        echo "Tampilkan gambar: <br> <img src='$path'>";
     }
 }
--- resources/views/file-upload.blade.php
+++ resources/views/file-upload.blade.php
@@ -20,6 +20,14 @@
             @csrf
 
             <div class="mb-3">
+                <label for="image_name" class="form-label">Nama Gambar</label>
+                <input type="text" class="form-control" id="image_name" name="image_name">
+                @error('image_name')
+                  <div class="text-danger">{{ $message }}</div>
+                @enderror
+              </div>
+
+            <div class="mb-3">
                 <label for="berkas" class="form-label">Gambar Profile</label>
                 <input type="file" class="form-control" id="berkas" name="berkas">
                 @error('berkas')
